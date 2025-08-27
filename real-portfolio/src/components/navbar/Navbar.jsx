3659e95b31f1aa09e0f0f1883c2d9910fd9d6162
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-08-27 11:31:59 -0300

    fix: rename Navbar component to NAvbar and update styling for improved consistency

--- real-portfolio/src/components/navbar/Navbar.jsx
+++ real-portfolio/src/components/navbar/Navbar.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+import photo from '../../assets/jole.png'
 
-const Navbar = ({navItems}) => {
+const NAvbar = ({navItems}) => {
   return (
     <>
     <Box sx={{
-        color: 'white',
-        bgcolor: 'blue',
+        color: '#D9D9D9',
+        bgcolor: '#2D2D2D',
+        border: 3,
+        borderColor: '#000000',
         padding: 2,
         display: 'flex',
         justifyContent: 'flex-end',
         flexDirection: 'row',
         gap: 4,
+        fontFamily: 'Fira Code, monospace',
+        fontSize: 25,
+        fontWeight: 'medium',
     }}>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'start',
+            alignItems: 'start',
+            mr: 'auto', // Pushes the image to the left
+
+        }}>
+            <Avatar
+                alt='photo'
+                src={photo}
+                sx={{
+                    width: 50,
+                    height: 50,
+                    mb: 0,
+                }}
+            />
+        </Box>
         {navItems.map((item, index) => (
             <Box key={index} component="span">{item}</Box>
         ))}
@@ -21,4 +45,4 @@ const Navbar = ({navItems}) => {
   )
 }
 
-export default Navbar
+export default NAvbar
