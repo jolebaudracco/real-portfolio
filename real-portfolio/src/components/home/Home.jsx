1b56471e9bb5924992d9155de993c2e6ba06573f
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-08-28 10:54:55 -0300

    feat: create Home component with MyButton integration for enhanced user interface

---
+++ real-portfolio/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '../buttons/MyButton'
+import MyButton from '../buttons/MyButton'
+import Box from '@mui/material/Box'
+
+const Home = () => {
+  return (
+    <>
+        <Box sx={{color: 'white'}}>Home</Box>
+        <MyButton/>
+    </>
+  )
+}
+
+export default Home
