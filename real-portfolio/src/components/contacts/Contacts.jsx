de577b0cab6afe461ad00b8ca2e40b328d3e355c
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-08-29 17:05:21 -0300

    feat: implement Contacts component with navigation to Home using MyButton

--- real-portfolio/src/components/contacts/Contacts.jsx
+++ real-portfolio/src/components/contacts/Contacts.jsx
@@ -1,8 +1,21 @@
+import Box from '@mui/material/Box'
 import React from 'react'
+import MyButton from '../buttons/MyButton'
 
-const Contacts = () => {
+const Contacts = ({setSelectedScreen}) => {
+  const handleClick = () => {
+    setSelectedScreen('Home')
+  }
   return (
-    <div>Contacts</div>
+    <>
+      <Box  onClick={handleClick} sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        mt: 5,
+      }}>
+        <MyButton text={'Home'}/>
+      </Box>
+    </>
   )
 }
 
