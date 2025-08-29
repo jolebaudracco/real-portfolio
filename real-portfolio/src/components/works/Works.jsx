9a2a8b344177fd2a6afbe72e1666d4fe64fc5a19
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-08-29 17:05:43 -0300

    feat: implement Works component with navigation to About Me using MyButton

--- real-portfolio/src/components/works/Works.jsx
+++ real-portfolio/src/components/works/Works.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import MyButton from '../buttons/MyButton'
+import Box from '@mui/material/Box'
 
-const Works = () => {
+const Works = ({setSelectedScreen}) => {
+
+  const handleClick = () => {
+    setSelectedScreen('About Me')
+  }
+
   return (
-    <div>Works</div>
+    <>
+      <Box onClick={handleClick} sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        mt: 5,
+      }}>
+        <MyButton text={'About me'}/>
+      </Box>
+    </>
   )
 }
 
