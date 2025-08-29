920be422a12782f13b5f642682b468d60d4ebde7
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-08-29 17:05:28 -0300

    feat: update Home component to include setSelectedScreen prop and enhance layout

--- real-portfolio/src/components/home/Home.jsx
+++ real-portfolio/src/components/home/Home.jsx
@@ -3,11 +3,29 @@ import Button from '../buttons/MyButton'
 import MyButton from '../buttons/MyButton'
 import Box from '@mui/material/Box'
 
-const Home = () => {
+const Home = ({setSelectedScreen}) => {
+
+const handleClick = () => {
+  setSelectedScreen('Works')
+}
   return (
     <>
-        <Box sx={{color: 'white'}}>Home</Box>
-        <MyButton/>
+        <Box sx={{
+          color: 'white',
+          fontFamily: 'Fira Code, monospace',
+          fontSize: 30,
+          textAlign: 'center',
+          mt: 10,
+        }}>
+          Jole Baudracco, a Web Developer and a QA Tester
+        </Box>
+        <Box onClick={handleClick} sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          mt: 5,
+        }}>
+          <MyButton  text={'Works'}/>
+        </Box>
     </>
   )
 }
